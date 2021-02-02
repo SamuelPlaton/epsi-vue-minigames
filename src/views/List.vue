835e4d70bdf139b62762c46142e9dcbe8b19054d
= <=>
2021-02-02 15:41:26 +0100

    feat: init list functionnality

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <table class="mx-auto">
+    <input
+      class="border border-gray-light m-4 px-2 py-1 rounded-lg"
+      v-model="searchString"
+      id="site-search"
+      name="q"
+      placeholder="Search country"
+    />
+    <table class="mx-auto w-3/4">
       <tr>
         <th class="border border-gray-light">Country</th>
         <th class="border border-gray-light">Region</th>
@@ -8,14 +15,20 @@
         <th class="border border-gray-light">Language</th>
       </tr>
       <tr
-        class="border border-gray-light"
-        v-for="country in countries"
+        class="border border-gray-light text-left"
+        v-for="country in filteredCountries"
         v-bind:key="country.name"
       >
-        <td class="border border-gray-light">{{ country.name }}</td>
-        <td class="border border-gray-light">{{ country.subregion }}</td>
-        <td class="border border-gray-light">{{ country.population }}</td>
-        <td class="border border-gray-light">
+        <td class="border border-gray-light pl-2 w-1/5">{{ country.name }}</td>
+        <td class="border border-gray-light pl-2 w-2/5">
+          {{ country.region }}, {{ country.subregion }}
+        </td>
+        <td class="border border-gray-light pl-2 w-1/5">
+          {{
+            country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+          }}
+        </td>
+        <td class="border border-gray-light pl-2 w-1/5">
           {{ country.languages[0].name }}
         </td>
       </tr>
@@ -26,19 +39,30 @@
 <script>
 import axios from "axios";
 
+// todo: filter by region
+
 export default {
   name: "List",
   data() {
     return {
-      countries: Array,
-      test: String
+      countries: [],
+      searchString: ""
     };
   },
-  beforeCreate() {
-    axios.get("https://restcountries.eu/rest/v2/all").then(res => {
-      console.log(res.data);
+  computed: {
+    filteredCountries() {
+      if (this.searchString.length === 0) {
+        return this.countries;
+      } else {
+        return this.countries.filter(country =>
+          country.name.toLowerCase().includes(this.searchString.toLowerCase())
+        );
+      }
+    }
+  },
+  async beforeCreate() {
+    await axios.get("https://restcountries.eu/rest/v2/all").then(res => {
       this.countries = res.data;
-      this.test = "Salut";
     });
   }
 };
